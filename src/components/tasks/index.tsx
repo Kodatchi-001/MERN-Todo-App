2982af77748c819df67f19472f4d8446c56fd22c
Ahmed Hariri <Ahmedhariri58@gmail.com>
2024-12-29 12:46:31 +0100

    Add design updates for new todo items display

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -3,17 +3,19 @@ import TextField from '@mui/material/TextField';
 
 export default function Tasks() {
     return <>
-        <section className="w-full h-[65vh] sm:h-[860px] 2xl:max-h-[830px] z-20 flex justify-center sm:pt-[9rem] relative">
-            <div className="w-[95%] sm:max-w-[750px] xl:max-w-[850px] flex flex-col gap-5 p-5 z-20 absolute rounded-xl mt-[-8vh] sm:mt-0 shadow-xl bg-white">
+        <section className="w-full h-[65vh] sm:max-h-[830px] z-20 flex justify-center sm:pt-[9rem] relative">
+            <div className="w-[95%] sm:max-w-[850px] flex flex-col gap-5 p-[17px] z-20 absolute rounded-xl mt-[-8vh] sm:mt-0 shadow-xl bg-white">
                 <div className="w-full h-full flex items-center gap-4">
                     <Box className="w-full">
                         <TextField fullWidth type="text" className="w-full" size="medium" label="Create New Task"
                             sx={{
                                 '& fieldset': {
                                     borderColor: 'gray',
+                                    border: '2px solid'
                                 },
                                 '& .MuiOutlinedInput-root.Mui-focused fieldset': {
                                     borderColor: 'gray',
+                                    border: '2px solid'
                                 },
                                 '& .MuiInputLabel-root': {
                                     color: 'black',
@@ -35,8 +37,35 @@ export default function Tasks() {
                         Add
                     </button>
                 </div>
-                <div>
+                <div className='flex flex-col gap-4'>
                     <li className='text-xl'>Tasks</li>
+                    <div>
+                        <ul className='flex justify-between items-center bg-gray-200 p-3 rounded-md'>
+                            <div className='flex items-center gap-3 text-xl'>
+                                <div className='px-[9px] py-[9px] border border-gray-500 rounded-[5px] cursor-pointer bg-white'></div>
+                                <li>Task</li>
+                            </div>
+                            <div className="w-2/6 h-full flex justify-end items-center gap-1">
+                                <i className="bx bxs-edit text-3xl cursor-pointer text-green-500"></i>
+                                <i className="bx bxs-x-square text-3xl cursor-pointer text-red-500"></i>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <li className='text-xl'>Tasks Checked</li>
+                    <div>
+                        <ul className='flex justify-between items-center bg-gray-200 p-3 rounded-md'>
+                            <div className='flex items-center gap-3 text-xl'>
+                                <div className='px-[9px] py-[9px] border border-gray-500 rounded-[5px] cursor-pointer bg-black'></div>
+                                <li>Task</li>
+                            </div>
+                            <div className="w-2/6 h-full flex justify-end items-center gap-1">
+                                <i className="bx bxs-edit text-3xl cursor-pointer text-green-500"></i>
+                                <i className="bx bxs-x-square text-3xl cursor-pointer text-red-500"></i>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
