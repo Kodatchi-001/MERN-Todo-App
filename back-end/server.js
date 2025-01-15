24a7aea6a0514047aca2a2347968e2e22c28fdab
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-01-15 16:21:48 +0100

    Create logic to generate Json-Web-Token

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require("cors");
 const mongoose = require('mongoose');
-const Tasks = require("./model/model.js");
+const Tasks = require("./model/taskModel.js");
+const Account = require("./model/accountModel.js");
 require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -102,6 +105,74 @@ app.put("/checked-task/:id", async (req, res) => {
         return res.status(500).json({ message: "Error updating checked tasks" });
     }
 });
+
+app.get("/accounts", async (req, res) => {
+    try {
+        const accounts = await Account.find();
+        if (accounts.length > 0) {
+            return res.status(200).json(accounts);
+        }
+        return res.status(200).json([]);
+    } catch (error) {
+        console.error("Error fetching accounts:", error);
+        return res.status(500).json({ message: "Error fetching accounts" });
+    }
+})
+
+app.post("/register", async (req, res) => {
+    const { id, fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+        return res.status(400).json({ message: "You don't have all information" });
+    }
+    try {
+        const existingAccount = await Account.findOne({ email });
+        if (existingAccount) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const userData = {
+            id: id,
+            fullName: fullName,
+            email: email,
+            password: hashedPassword
+        };
+        const token = jwt.sign(userData, process.env.JWT_SECRET);
+        if (token) {
+            const newAccount = new Account(userData);
+            await newAccount.save();
+            return res.status(201).json({ message: 'Account has been created!', token });
+        }
+    } catch (error) {
+        console.error("Error creating account:", error);
+        return res.status(500).json({ message: "Error creating account" });
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "Information account is required" });
+    }
+    try {
+        const existingAccount = await Account.findOne({ email });
+        if (!existingAccount) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, existingAccount.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const userData = {
+            id: existingAccount.id,
+            email: existingAccount.email
+        };
+        const token = jwt.sign(userData, process.env.JWT_SECRET);
+        return res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        console.error("Error logging:", error);
+        return res.status(500).json({ message: "Error logging" });
+    }
+})
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
